012d60e9a2b63e9c4624b07450ce330b98971ef8
misha <easydriverup@gmail.com>
2020-06-06 18:13:44 -0500

    Updates

---
+++ precache-manifest.35545f18b4205c264ade8f71e8de4a72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5647d2028b88f98eaf5f9b85afd74e5e",
+    "url": "/rusbur/KanbanProject.git/index.html"
+  },
+  {
+    "revision": "f908c9f1dd080f7558c3",
+    "url": "/rusbur/KanbanProject.git/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2a64257a0a6d2d29ee8c",
+    "url": "/rusbur/KanbanProject.git/static/css/main.5430f7a4.chunk.css"
+  },
+  {
+    "revision": "f908c9f1dd080f7558c3",
+    "url": "/rusbur/KanbanProject.git/static/js/2.e2d54959.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rusbur/KanbanProject.git/static/js/2.e2d54959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a64257a0a6d2d29ee8c",
+    "url": "/rusbur/KanbanProject.git/static/js/main.f4f0f797.chunk.js"
+  },
+  {
+    "revision": "5b0a45d2b6a0e96d87c8",
+    "url": "/rusbur/KanbanProject.git/static/js/runtime-main.08b1d0e9.js"
+  }
+]);
